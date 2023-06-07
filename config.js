0e5f525cfc794091064eb40d5aaf083f9ba6efc1
helmihy <helmihy6@gmail.com>
2023-06-07 23:03:16 +0700

    mengubah host db

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     jwtSecret: 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY4NjE1MjAwMywiaWF0IjoxNjg2MTUyMDAzfQ.psGlhK3SxnsCbZQgvtfK1R00bV8ukGq0ayNbb5d5QWc', // https://www.javainuse.com/jwtgenerator HS256 c23@ps261
     dbConfig: {
       connectionLimit: 10,
-      host: 'localhost',
+      host: '34.101.195.31',
       user: 'root',
       password: 'c23@ps261',
       database: 'botaniplan',
